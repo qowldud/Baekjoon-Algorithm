bc237ed8459681b23656ee59bb0a927c37f4575a
qowldud <120749541+qowldud@users.noreply.github.com>
2023-06-24 13:53:56 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string s;
+    while(true){
+        getline(cin,s);
+        if(s=="")break;
+        cout<<s<<endl;
+    }
+    return 0;
+}
