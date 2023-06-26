0ad59d69ca5c62bd771fa66b185165107fb3d184
qowldud <120749541+qowldud@users.noreply.github.com>
2023-06-27 04:53:06 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int num;
+    cin>>num;
+    for(int i=0;i<num;i++){
+        int a;
+        cin>>a;
+        cout<<a<<" "<<a<<endl;
+    }
+    return 0;
+}
