2eba8090de9d940e9f6b1cb287f4e7927ef6e975
qowldud <120749541+qowldud@users.noreply.github.com>
2023-07-04 19:41:45 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+ int main(void){
+    string word;
+    cin>>word;
+    cout<<":fan:"<<":fan:"<<":fan:"<<'\n';
+    cout<<":fan:"<<":"<<word<<":"<<":fan:"<<'\n';
+    cout<<":fan:"<<":fan:"<<":fan:"<<endl;
+    return 0;
+ }
