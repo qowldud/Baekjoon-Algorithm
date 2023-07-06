e95ce6bc3fbe6ecef1fc6254c5505f125b22174b
qowldud <120749541+qowldud@users.noreply.github.com>
2023-07-07 00:51:13 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -1,24 +1,11 @@
 #include <iostream>
-#include <string>
 #include <algorithm>
 using namespace std;
-
-bool compare(int a, int b){
-    return a>b;
-}
 
 int main(void){
     string num;
     cin>>num;
-    int a = num.length();
-    int array[a];
-    for(int i=0;i<a;i++){
-        array[i]=stoi(num.substr(i,1));
-    }
-    sort(array,array+a,compare);
-    for(int i=0;i<a;i++){
-        cout<<array[i];
-    }
-    cout<<'\n';
+    sort(num.begin(),num.end(),greater<char>());
+    cout<<num;
     return 0;
 }
