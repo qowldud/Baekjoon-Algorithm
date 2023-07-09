d72b4f13aa1195912c9ffc11a4c781a9d00c5533
qowldud <120749541+qowldud@users.noreply.github.com>
2023-07-09 19:33:01 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int A,B;
+    cin>>A>>B;
+    int a,b;
+    for(int i=0;i<A;i++){
+        cin>>a>>b;
+    }
+    cout<<"비와이"<<'\n';
+    return 0;
+}
