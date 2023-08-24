4bee146c35a32dd48073c822e62851b90b469811
qowldud <120749541+qowldud@users.noreply.github.com>
2023-08-24 18:55:30 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 252 ms, Memory: 10896 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N,M,R,cou = 1;
+vector<int> v[100001];
+bool visit[100001];
+int arr[100001];
+void dfs(int cur){
+    visit[cur] = true;
+    arr[cur] = cou;
+    cou++;
+    for(int k : v[cur]){
+        if(!visit[k])dfs(k);
+    }
+}
+int main(void){
+    cin>>N>>M>>R;
+    for(int i=0;i<M;i++){
+        int a,b;;
+        cin>>a>>b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    for(int i=1;i<=N;i++){
+        sort(v[i].begin(),v[i].end(),greater<>());
+    }
+    dfs(R);
+    for(int i=1;i<=N;i++){
+        cout<<arr[i]<<'\n';
+    }
+}
