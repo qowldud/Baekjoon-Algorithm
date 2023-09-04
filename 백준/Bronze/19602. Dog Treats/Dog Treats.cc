8cc8350265868deb9e24006b26a542b48c3a6c78
qowldud <120749541+qowldud@users.noreply.github.com>
2023-09-04 13:46:57 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a,b,c;
+    int sum = 0;
+    cin>>a>>b>>c;
+    sum = a*1+b*2+c*3;
+    if(sum>=10)cout<<"happy";
+    else cout<<"sad";
+}
