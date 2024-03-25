c62f0754c6f51955b0ddc9ac8f372ad237b16e33
qowldud <120749541+qowldud@users.noreply.github.com>
2024-03-25 23:35:21 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int num;
+    cin>>num;
+    
+    for(int i=0;i<num;i++){
+        int a,b;
+        char c;
+        cin>>a>>c>>b;
+        cout<<a+b<<'\n';
+    }
+}
