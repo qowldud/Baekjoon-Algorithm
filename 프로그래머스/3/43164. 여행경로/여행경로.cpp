1a0945fb5403c419477b2aa62c49b2603d74c950
qowldud <120749541+qowldud@users.noreply.github.com>
2024-06-24 14:08:51 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -11,7 +11,7 @@ map<string, int> m;
 vector<vector<string>> v(10001);
 vector<vector<bool>> BOOL(10001);
 int k;
-int found = false;
+bool found = false;
 
 void dfs(string s, vector<string>& answer){
     answer.push_back(s);
@@ -24,7 +24,7 @@ void dfs(string s, vector<string>& answer){
         if(!BOOL[n][i] && !found){
             BOOL[n][i] = true;
             dfs(v[n][i], answer);
-            if(found) return;
+            if(found) return ;
             BOOL[n][i] = false;
         }
 
@@ -38,6 +38,7 @@ vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
     int count = 0;
     int number;
+    
     for(int i=0;i<tickets.size();i++){
         if(m.find(tickets[i][0]) == m.end()){
             m.insert({tickets[i][0], count});
@@ -51,7 +52,7 @@ vector<string> solution(vector<vector<string>> tickets) {
 
         } else{
             v[m[tickets[i][0]]].push_back(tickets[i][1]);
-            BOOL[count].push_back(false);
+            BOOL[m[tickets[i][0]]].push_back(false);
             if(m.find(tickets[i][1]) == m.end()){
                 m.insert({tickets[i][1], count});
                 count++;
