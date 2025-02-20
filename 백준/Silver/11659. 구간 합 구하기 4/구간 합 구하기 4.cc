7e2c4e0b0ca15dc40e3b987c658369ec89434d32
qowldud <120749541+qowldud@users.noreply.github.com>
2025-02-20 14:33:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,23 +1,25 @@
 #include <iostream>
+
 using namespace std;
 
 int main(void){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int num1,num2;
-    cin>>num1>>num2;
-    int array[num1];
-    int array1[num1];
-    for(int i=0;i<num1;i++){
-        cin>>array[i];
-        if(i==0)array1[i]=array[i];
-        else array1[i]=array[i]+array1[i-1];
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N,M;
+    cin>>N>>M;
+    int S[N+1] ={};
+    
+    for(int i=1;i<=N;i++){
+        int a;
+        cin>>a;
+        S[i] = S[i-1] + a;
     }
-    for(int i=0;i<num2;i++){
+    
+    for(int i=0;i<M;i++){
         int a,b;
         cin>>a>>b;
-        if(a==1)cout<<array1[b-1]<<'\n';
-        else cout<<array1[b-1]-array1[a-2]<<'\n';
+        cout<<S[b] - S[a-1]<<'\n';
     }
 }
