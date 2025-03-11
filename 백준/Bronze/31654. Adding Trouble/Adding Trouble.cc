3d0a5205b86520feeea0ef85893b000ddf5d833e
qowldud <120749541+qowldud@users.noreply.github.com>
2025-03-11 10:58:28 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(a+b == c){
+        cout<<"correct!";
+    }else{
+        cout<<"wrong!";
+    }
+}
