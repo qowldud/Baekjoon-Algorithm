531e289f682ae1e3a1c91ba12a144a7773eb82c3
qowldud <120749541+qowldud@users.noreply.github.com>
2025-03-22 16:30:58 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(void){
+    float a,b;
+    cin>>a>>b;
+    float k = a*b/2;
+    
+    cout<<fixed;
+    cout<<setprecision(1);
+    cout<<k;
+    
+}
