bfbcfe55d1bc4f6368f076fdb324a48b50e638d6
배지영 <120749541+qowldud@users.noreply.github.com>
2025-12-26 12:58:48 +0900

    [level 1] Title: 카드 뭉치, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,18 +1,50 @@
+class Queue{
+    items = [];
+    front = 0;
+    rear = 0;
+    
+    constructor(array){
+        this.items = array;
+        this.rear = array.length;
+    }
+    
+    push(item){
+        this.items.push(item);
+        this.rear++;
+    }
+    pop(item){
+        return this.items[this.front++];
+    }
+    
+    first(){
+        return this.items[this.front];
+    }
+    
+    isEmpty(){
+        return this.front === this.rear;
+    }
+}
+
 function solution(cards1, cards2, goal) {
     var answer = '';
     
-    for(let i = 0; i < goal.length; i++){
-        if(cards1.length && cards1[0] === goal[i]){
-            cards1.shift();
-        }else if(cards2.length && cards2[0] === goal[i]){
-            cards2.shift();
-        }else{
-            answer = "No";
+    cards1 = new Queue(cards1);
+    cards2 = new Queue(cards2);
+    goal = new Queue(goal);
+    
+    while(!goal.isEmpty()){
+        if(!cards1.isEmpty() && cards1.first() === goal.first()){
+            cards1.pop();
+            goal.pop();
+        } else if(!cards2.isEmpty() && cards2.first() === goal.first()){
+            cards2.pop();
+            goal.pop();
+        } else{
             break;
         }
     }
     
-    if(!answer) answer = "Yes";
+    answer = goal.isEmpty() ? "Yes" : "No";
     
     return answer;
 }
