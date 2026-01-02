e39340f8d2cced1ce3bfcfa0cecf01046d2f8a88
배지영 <120749541+qowldud@users.noreply.github.com>
2026-01-02 16:49:01 +0900

    [level 2] Title: 할인 행사, Time: 197.46 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,31 +1,35 @@
+function isEqual(object1, object2){
+    const objectKey1 = Object.keys(object1);
+    const objectKey2 = Object.keys(object2);
+    
+    if(objectKey1.length !== objectKey2.length) return false;
+    
+    for(const key of objectKey1){
+        if(object1[key] !== object2[key]) return false;
+    }
+     return true;
+}
+
 function solution(want, number, discount) {
     var answer = 0;
     
-    const w = {};
-
+    const wantObj = {};
     
-    for(let i=0; i < discount.length - 9; i++){
-        
-        let bool = false;
-        
-        for(let i = 0; i < want.length; i++){
-            w[want[i]] = number[i];
-            }
-        
-        for(let j = i; j < i + 10; j++){
-            if(w[discount[j]]){
-                w[discount[j]]--;
+    for(let i = 0; i < want.length; i++){
+        wantObj[want[i]] = number[i];
+    }
+    
+    for(let i = 0; i < discount.length - 9; i++){
+        const discount10d = {};
+        for(let j = i; j < i + 10; j ++){
+            if(wantObj[discount[j]]){
+                discount10d[discount[j]] = (discount10d[discount[j]] || 0 ) + 1;
             }
         }
         
-        for(let j = 0; j < want.length; j++){
-            if(w[want[j]]){
-                bool = true;
-                break;
-            }
+        if(isEqual(discount10d, wantObj)){
+            answer++;
         }
-        
-        if(!bool)answer++;
     }
     
     return answer;
