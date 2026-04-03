29c904293cfebcdcd43456b9b717bbd25c1a3287
배지영 <120749541+qowldud@users.noreply.github.com>
2026-04-03 19:15:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,11 +1,3 @@
 function solution(A, B) {
-    const arrA = A.split("");
-    const arrB = B.split("");
-    
-    for(let i = 0; i < arrA.length; i++){
-        if(JSON.stringify(arrA) === JSON.stringify(arrB)) return i;
-        const c = arrA.pop();
-        arrA.unshift(c);
-    }
-    return -1;
+    return (B + B).indexOf(A);
 }
