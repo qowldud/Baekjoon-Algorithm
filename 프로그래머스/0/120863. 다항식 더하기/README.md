503c8513576b09fa29ac64582230adf78286850e
배지영 <120749541+qowldud@users.noreply.github.com>
2026-04-03 20:47:23 +0900

    [level 0] Title: 다항식 더하기, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/README.md
+++ 프로그래머스/0/120863. 다항식 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다항식 더하기 - 120863 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120863#) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.10 ms
+메모리: 33.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 20:38:22
+2026년 04월 03일 20:47:21
 
 ### 문제 설명
 
--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,15 +1,18 @@
 function solution(polynomial) {
-    const arr = polynomial.split(" + ");
-    let a = 0;
-    let b = 0;
+    const terms = polynomial.split(" + ");
+    let xSum = 0;
+    let numSum = 0;
     
-    for(const str of arr){
+    for(const str of terms){
         if(str.includes("x")){
-            a += str.length === 1 ? 1 : Number(str.slice(0, str.length - 1));
+            xSum += str === "x" ? 1 : Number(str.slice(0, -1));
         }else{
-            b += Number(str);
+            numSum += Number(str);
         }
     }
     
-    return a ? b ?`${a !== 1 ? a : ''}x + ${b}`: `${a !== 1 ? a : ''}x` : `${b}`;
+    const xTerm = xSum ? `${xSum === 1 ? "" : xSum}x` : "";
+    const numTerm = numSum ? String(numSum) : "";
+    
+    return xTerm && numTerm ? `${xTerm} + ${numTerm}` : xTerm || numTerm
 }
