aaa5e52b095364f18284ed228b35e7654f7fda87
배지영 <120749541+qowldud@users.noreply.github.com>
2026-04-04 09:50:54 +0900

    [level 0] Title: 특이한 정렬, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,18 +1,3 @@
 function solution(numlist, n) {
-   let fars = numlist.map((num) => n - num).sort((a, b) => Math.abs(a) - Math.abs(b));
-    
-   fars.map((far, idx) => {
-        if((idx !== fars.length - 1) && (Math.abs(far) === Math.abs(fars[idx +1]))){
-            fars[idx] = Math.abs(far);
-            fars[idx + 1] = -Math.abs(far);
-    }
-})
-    
-    const answer = fars.map((far, idx) => { 
-        if(fars.filter((num, i) => i !== idx && Math.abs(num) === Math.abs(far)).length) return n + far;
-        else return n - far;
-    });
-    
-    return answer;
-                                                  
+    return numlist.sort((a, b) => Math.abs(n - a) - Math.abs(n - b) || b - a);
 }
