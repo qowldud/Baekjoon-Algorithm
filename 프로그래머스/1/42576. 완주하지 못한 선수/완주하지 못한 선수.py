971b8c284392ed19e8bc12bc52078b56ee08a9d0
배지영 <120749541+qowldud@users.noreply.github.com>
2026-06-23 19:42:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.34 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,5 @@
+import collections
+
 def solution(participant, completion):
-    dic = {}
-    for part in participant:
-        dic[part] = dic.get(part, 0) + 1
-        
-    for name in completion:
-        if name in dic:
-            dic[name] -= 1
-            if(dic[name] == 0):
-                del dic[name]
-            
-    for key in dic:
-        return key
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
