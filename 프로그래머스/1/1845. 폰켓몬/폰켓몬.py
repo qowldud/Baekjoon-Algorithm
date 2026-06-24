abd93a9fcf2cb6db581133182390bedd99b64c80
배지영 <120749541+qowldud@users.noreply.github.com>
2026-06-24 17:21:59 +0900

    [level 1] Title: 폰켓몬, Time: 0.63 ms, Memory: 12.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,10 +1,2 @@
-import collections
-
 def solution(nums):
-    li = collections.Counter(nums)
-    n = len(nums)
-    
-    if n//2 >= len(li):
-        return len(li)
-    else:
-        return n//2
+    return min(len(nums)//2, len(set(nums)))
