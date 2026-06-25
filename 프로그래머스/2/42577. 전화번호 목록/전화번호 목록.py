59ce3597255801e5d6fa2d82ce6f5636432ee01d
배지영 <120749541+qowldud@users.noreply.github.com>
2026-06-25 10:11:14 +0900

    [level 2] Title: 전화번호 목록, Time: 457.97 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,9 +1,11 @@
 def solution(phone_book):
-    phone_book.sort()
-    n = len(phone_book)
-
-    for i in range(n-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+    dic = {}
+    for num in phone_book:
+        dic[num] = True
+    
+    for num in phone_book:
+        for i in range(len(num)-1):
+            if dic.get(num[:i+1]):
                 return False
-            
+    
     return True
