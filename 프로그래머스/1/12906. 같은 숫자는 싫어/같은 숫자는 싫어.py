87af148178418f2536235234cf368c756ee377c1
배지영 <120749541+qowldud@users.noreply.github.com>
2026-06-28 12:41:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.67 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,8 @@
 def solution(arr):
-    stack = []
-    stack.append(arr[0])
+    stack = [arr[0]]
+
     for num in arr[1:]:
-        if stack[len(stack)-1] != num:
+        if stack[-1] != num:
             stack.append(num)
             
     return stack
