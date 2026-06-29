508ee087160fc42871a8a73c7fa99ba30f3ad946
배지영 <120749541+qowldud@users.noreply.github.com>
2026-06-29 10:35:17 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,25 +1,20 @@
 import math
 
 def solution(progresses, speeds):
-    left_progresses = []
-    days = []
-    max = 0
+    answer = []
+
+    current_release_day = math.ceil((100 - progresses[0]) / speeds[0])
     count = 1
-    for progress in progresses:
-        left_progresses.append(100 - progress)
-        
-    max = math.ceil(left_progresses[0]/speeds[0])
-    
-    for (progresse, speed) in zip(left_progresses[1:], speeds[1:]):
-        day = math.ceil(progresse/speed)
-        
-        if max >= day:
-            count+=1
-        else:   
-            days.append(count)
-            max = day
+
+    for progress, speed in zip(progresses[1:], speeds[1:]):
+        day = math.ceil((100 - progress) / speed)
+
+        if day <= current_release_day:
+            count += 1
+        else:
+            answer.append(count)
+            current_release_day = day
             count = 1
-    
-    days.append(count)
-    return days
-    
+
+    answer.append(count)
+    return answer
