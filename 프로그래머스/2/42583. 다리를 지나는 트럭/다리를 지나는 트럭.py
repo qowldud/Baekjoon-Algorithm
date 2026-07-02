a0e08e95931df530af66ebd20f752b89cde3cdfd
배지영 <120749541+qowldud@users.noreply.github.com>
2026-07-02 14:06:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 195.61 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -3,7 +3,7 @@ def solution(bridge_length, weight, truck_weights):
     s = []
     answer = 0
     
-    while truck_weights:
+    while truck_weights or s:
         answer+=1
         for i in range(len(s)):
             s[i][1] -= 1
@@ -11,18 +11,11 @@ def solution(bridge_length, weight, truck_weights):
             current_weight -= s[0][0]
             s.pop(0)
         
-        if current_weight + truck_weights[0] <= weight and len(s) < bridge_length:
+        if truck_weights and current_weight + truck_weights[0] <= weight and len(s) < bridge_length:
             current_weight += truck_weights[0]
             s.append([truck_weights[0], bridge_length])
             truck_weights.pop(0)
 
-    while s:
-        answer+=1
-        for i in range(len(s)):
-            s[i][1] -= 1
-        
-        if s[0][1] == 0:
-            s.pop(0)
     return answer
         
     
